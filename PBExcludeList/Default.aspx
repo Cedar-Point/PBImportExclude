bda617365cea83ea0f136243204c249485f1d7ec
Bickerstaff <dylan.bickerstaff@ad.cedarfair.com>
2021-04-12 16:50:50 -0400

    Add project files.

---
+++ PBExcludeList/Default.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="PBExcludeList.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>PB Exlusions</title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 26px;
+        }
+        .auto-style2 {
+            height: 0px;
+            margin-top: 20px;
+            margin-bottom: 24px;
+        }
+        .auto-style3 {
+            width: 111px;
+        }
+        .auto-style4 {
+            height: 26px;
+            width: 111px;
+        }
+        .auto-style5 {
+            margin-top: 0px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Phone Book Administrator - Exclusions List</h1>
+    <form id="form1" runat="server">
+        <hr class="auto-style2" />
+        <asp:Table ID="tblDataList" runat="server" GridLines="Both">
+            <asp:TableRow runat="server" TableSection="TableHeader">
+                <asp:TableCell runat="server"></asp:TableCell>
+                <asp:TableCell runat="server" Font-Bold="True">Start Range</asp:TableCell>
+                <asp:TableCell runat="server" Font-Bold="True">End Range</asp:TableCell>
+                <asp:TableCell runat="server" Font-Bold="True">Description</asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>
+        <hr class="auto-style2" />
+        <asp:Label ID="lblAddError" runat="server" EnableViewState="False" Font-Bold="True" ForeColor="#FF3300" Height="40px" Text="Please make sure all fields have a value." Visible="False" Width="360px"></asp:Label>
+        <table style="width:100%;" id="tblEditTable">
+            <tr>
+                <td class="auto-style3">Start Range</td>
+                <td>
+                    <asp:TextBox ID="tbStartRange" runat="server" TextMode="Number" Width="100px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style3">End Range</td>
+                <td>
+                    <asp:TextBox ID="tbEndRange" runat="server" TextMode="Number" Width="100px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4">Description</td>
+                <td class="auto-style1">
+                    <asp:TextBox ID="tbDesc" runat="server" Width="300px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style4"></td>
+                <td class="auto-style1">
+                    <asp:Button ID="btnAddSet" runat="server" OnClick="btnAddSet_Click" Text="Add" />
+                </td>
+            </tr>
+        </table>
+        <hr class="auto-style2" />
+        <p>
+            <asp:Button ID="btnCancel" runat="server" CssClass="auto-style5" OnClick="btnCancel_Click" Text="Cancel" Width="113px" />
+&nbsp;<asp:Button ID="btnSave0" runat="server" CssClass="auto-style5" OnClick="btnSave0_Click" Text="Save" Width="113px" />
+        </p>
+    </form>
+
+</body>
+</html>
---
+++ PBExcludeList/Default.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.IO;
+
+namespace PBExcludeList
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private PBExlusionList ExlusionList = new PBExlusionList();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.Session["PBExclusion"] == null)
+            {
+                if (File.Exists(Properties.Settings.Default.PBExclusionListXMLPath))
+                {
+                    FileStream file = File.OpenRead(Properties.Settings.Default.PBExclusionListXMLPath);
+                    XDocument doc = XDocument.Load(file);
+                    file.Close();
+                    List<XElement> PNs = doc.Root.Descendants("PN").ToList();
+                    foreach (XElement pn in PNs)
+                    {
+                        ExlusionList.Add(new PBExlusion(pn.Attribute("From").Value, pn.Attribute("To").Value, pn.Attribute("Desc").Value));
+                    }
+                }
+                Page.Session["PBExclusion"] = ExlusionList;
+            }
+            else
+            {
+                ExlusionList = (PBExlusionList)Page.Session["PBExclusion"];
+            }
+            LoadExlusionList();
+        }
+        private void LoadExlusionList()
+        {
+            for (int c = 1; c < tblDataList.Rows.Count; c++)
+            {
+                tblDataList.Rows.RemoveAt(c);
+            }
+            foreach (PBExlusion exl in ExlusionList)
+            {
+                TableRow tr = new TableRow();
+                int exlIndex = tblDataList.Rows.Add(tr);
+                TableCell cbCell = new TableCell();
+                Button button = new Button();
+                button.Text = "Remove";
+                button.Attributes.Add("exlIndex", (exlIndex - 1).ToString());
+                button.Click += RemoveButton_Click;
+                cbCell.Controls.Add(button);
+                tr.Cells.Add(cbCell);
+                tr.Cells.Add(new TableCell()
+                {
+                    Text = exl.StartRange.ToString()
+                });
+                tr.Cells.Add(new TableCell()
+                {
+                    Text = exl.EndRange.ToString()
+                });
+                tr.Cells.Add(new TableCell()
+                {
+                    Text = exl.Description
+                });
+            }
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (!int.TryParse(btn.Attributes["exlIndex"], out int index)) return;
+            ExlusionList.RemoveAt(index);
+            Response.Redirect("./Default.aspx");
+        }
+
+        protected void btnAddSet_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(tbStartRange.Text, out _) && int.TryParse(tbEndRange.Text, out _) && tbDesc.Text != "")
+            {
+                ExlusionList.Add(new PBExlusion(tbStartRange.Text, tbEndRange.Text, tbDesc.Text));
+                Response.Redirect("./Default.aspx");
+            }
+            else
+            {
+                lblAddError.Visible = true;
+            }
+        }
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Page.Session["PBExclusion"] = null;
+            Response.Redirect("./Default.aspx");
+        }
+        protected void btnSave0_Click(object sender, EventArgs e)
+        {
+            XDocument xdoc = new XDocument(new XElement("PBExlusions"));
+            foreach(PBExlusion exl in ExlusionList)
+            {
+                XElement xelm = new XElement("PN");
+                xelm.Add(new XAttribute("From", exl.StartRange));
+                xelm.Add(new XAttribute("To", exl.EndRange));
+                xelm.Add(new XAttribute("Desc", exl.Description));
+                xdoc.Root.Add(xelm);
+            }
+            if (File.Exists(Properties.Settings.Default.PBExclusionListXMLPath)) File.Delete(Properties.Settings.Default.PBExclusionListXMLPath);
+            FileStream stream = File.OpenWrite(Properties.Settings.Default.PBExclusionListXMLPath);
+            xdoc.Save(stream);
+            stream.Close();
+        }
+    }
+    public class PBExlusionList : List<PBExlusion> { }
+    public class PBExlusion
+    {
+        public PBExlusion() { }
+        public PBExlusion(string StartRange, string EndRange, string Description)
+        {
+            this.StartRange = StartRange;
+            this.EndRange = EndRange;
+            this.Description = Description;
+        }
+        public string StartRange = "";
+        public string EndRange = "";
+        public string Description = "";
+    }
+}
